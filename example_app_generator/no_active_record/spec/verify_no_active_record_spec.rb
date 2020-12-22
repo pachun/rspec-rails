1ccfe5c761b17a22233ba2af635423ddb4e06801
Phil Pirozhkov <hello@fili.pp.ru>
2020-12-22 19:35:11 +0300

    wip! Attempt to add a failing test case

--- example_app_generator/no_active_record/spec/verify_no_active_record_spec.rb
+++ example_app_generator/no_active_record/spec/verify_no_active_record_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
+class SomeClass; end
+
 RSpec.describe 'Example App' do
   it "does not have ActiveRecord defined" do
     expect(defined?(ActiveRecord)).not_to be
+  end
+
+  it "does not crash on Rails 6.1.0", issue: 2417 do
+    SomeClass.new
   end
 end
